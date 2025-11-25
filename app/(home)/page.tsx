e99dcd2b03448836ff7a8541b36a346a378571a9
lih-star <dlagyekffu55@gmail.com>
2025-11-25 15:10:53 +0900

    data fetching 다른 방법, loading페이지 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,16 @@
-import { Metadata } from "next";
-
-export const metadata : Metadata = {
-    title: 'Home'
+export const metadata = {
+    title: "Home"
 }
 
-export default function Page() {
+async function getMovies() {
+    return await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+        .then(response => response.json());
+}
+export default async function HomePage() {
+    const movies = await getMovies();
     return (
         <div>
-            <h1>Hello!</h1>
+            {JSON.stringify(movies)}
         </div>
     );
 }
