820cf8f071033defe8b0d8d93bdf98e4afba91a6
lih-star <dlagyekffu55@gmail.com>
2025-11-25 16:33:20 +0900

    Promise.all을 활용한 await 병렬 처리

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,4 +1,19 @@
+import { get } from "http";
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideo(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
 export default async function MovieDetail({ params }: { params: { id: string } }) {
   const { id } = await params;
-  return <h1>Movie {id}</h1>;
+  const movie = await getMovie(id);
+  const video = await getVideo(id);
+  return <h1>{movie.title}</h1>;
 }
